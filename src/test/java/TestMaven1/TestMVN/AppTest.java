291881f979c175c3700e12782783b7776268faac
jaydeep8511 <jaydeepprajapati24@gmail.com>
2019-04-29 16:10:58 +0530

    converted into testng

--- src/test/java/TestMaven1/TestMVN/AppTest.java
+++ src/test/java/TestMaven1/TestMVN/AppTest.java
@@ -1,40 +1,15 @@
 package TestMaven1.TestMVN;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
+
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void sts() {
+		System.out.println("test app ...");
+	}
 }
